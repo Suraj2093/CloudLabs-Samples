c189bda1d49975133167ba1fe9b563fcc0151a66
Suraj Pai <suraj.rpai2093@gmail.com>
2021-05-14 23:00:31 +0530

    Updated script for persistency of mounting

--- vm-linux-datadisk/script1.sh
+++ vm-linux-datadisk/script1.sh
@@ -8,10 +8,6 @@ echo "Counter : " $counter >> mountdatadisk.log
 
 
 arr=(sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm sdn sdo sdp sdq sdr sds sdt sdu sdv sdw sdx sdy sdz)
-echo "Array : " ${arr[*]} >> mountdatadisk.log
-
-echo "Counter : " $counter >> mountdatadisk.log
-
 
 while [ $counter -lt ${No_of_DataDisk} ]; do
 echo "Disk Partition : " ${arr[$counter]} >> mountdatadisk.log
@@ -28,17 +24,23 @@ EOF
 mkfs.ext4 /dev/${arr[$counter]}1 <<EOF
 y
 EOF
+sleep 2
 
 #"creating Directories to mount "
 mkdir -p /mnt/data$counter
+sleep 2
+
+#log "Fetching the UUID of the device"
+uuid=$(blkid -s UUID -o value /dev/${arr[$counter]}1)
 
 #log "Adding partitions to fstab to mount on reboot"
-echo "/dev/${arr[$counter]}1     /mnt/data$counter    ext4   defaults,nofail   0 2" >> /etc/fstab
-
-#log "Mounting newly created partition using fstab file"
-mount -a
-sleep 10
+echo "UUID=$uuid     /mnt/data$counter    ext4   defaults,nofail   0 2" >> /etc/fstab
+sleep 2
 
 counter=`expr $counter + 1`
 echo "Counter : " $counter >> mountdatadisk.log
 done
+
+#log "Mounting newly created partition using fstab file"
+mount -a
+sleep 10
